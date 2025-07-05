30413c2faaa75cf390ab35c3e81c9747ed49e4f9
Amari Garrett <amariwgarrett@gmail.com>
2025-07-05 03:54:05 -0400

    Create repositories for the Task and Task List

---
+++ src/main/java/com/amariwyking/tasks/repositories/TaskListRepository.java
@@ -0,0 +1,11 @@
+package com.amariwyking.tasks.repositories;
+
+import com.amariwyking.tasks.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+}
