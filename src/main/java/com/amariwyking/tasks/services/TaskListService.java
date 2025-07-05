0f68007ff56ae95400823f1298a2d5a11f059e15
Amari Garrett <amariwgarrett@gmail.com>
2025-07-05 15:04:32 -0400

    Implement the retrieval of an individual task list by its id

--- src/main/java/com/amariwyking/tasks/services/TaskListService.java
+++ src/main/java/com/amariwyking/tasks/services/TaskListService.java
@@ -4,8 +4,11 @@ import com.amariwyking.tasks.controllers.TaskListController;
 import com.amariwyking.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
 }
