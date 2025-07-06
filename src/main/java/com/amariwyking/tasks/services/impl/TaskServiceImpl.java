2c3835781b9e273e5c1cffbdedb06bb717a040e6
Amari Garrett <amariwgarrett@gmail.com>
2025-07-06 00:10:52 -0400

    - Implement the creation of a Task - Change the target database

--- src/main/java/com/amariwyking/tasks/services/impl/TaskServiceImpl.java
+++ src/main/java/com/amariwyking/tasks/services/impl/TaskServiceImpl.java
@@ -1,13 +1,18 @@
 package com.amariwyking.tasks.services.impl;
 
 import com.amariwyking.tasks.domain.entities.Task;
+import com.amariwyking.tasks.domain.entities.TaskList;
+import com.amariwyking.tasks.domain.entities.TaskPriority;
+import com.amariwyking.tasks.domain.entities.TaskStatus;
 import com.amariwyking.tasks.repositories.TaskListRepository;
 import com.amariwyking.tasks.repositories.TaskRepository;
 import com.amariwyking.tasks.services.TaskListService;
 import com.amariwyking.tasks.services.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -23,5 +28,37 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public List<Task> listTasks(UUID taskListId) {
         return taskRepository.findByTaskListId(taskListId);
+    }
+
+    @Override
+    public Task createTask(UUID taskListId, Task task) {
+        if (null != task.getId()) {
+            throw new IllegalArgumentException(String.format("Task already has ID: %s", task.getId()));
+        }
+
+        if (null == task.getTitle() || task.getTitle().isBlank()) {
+            throw new IllegalArgumentException("Task must have a title");
+        }
+
+        TaskPriority priority = Optional.ofNullable(task.getPriority()).orElse(TaskPriority.MEDIUM);
+
+        TaskList taskList = taskListRepository
+                .findById(taskListId)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("Invalid Task List ID provided")
+                );
+
+        LocalDateTime now = LocalDateTime.now();
+        return taskRepository.save(new Task(
+                null,
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                TaskStatus.OPEN,
+                priority,
+                taskList,
+                now,
+                now
+        ));
     }
 }
