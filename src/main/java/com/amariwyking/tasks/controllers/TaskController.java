a4049f97bb9bb70c2c7935b8705ad2a994af3379
Amari Garrett <amariwgarrett@gmail.com>
2025-07-06 00:24:14 -0400

    Implement retrieval of an individual task using its ID

--- src/main/java/com/amariwyking/tasks/controllers/TaskController.java
+++ src/main/java/com/amariwyking/tasks/controllers/TaskController.java
@@ -7,6 +7,7 @@ import com.amariwyking.tasks.services.TaskService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -40,5 +41,13 @@ public class TaskController {
         );
 
         return taskMapper.toDto(createdTask);
+    }
+
+    @GetMapping("/{task_id}")
+    public Optional<TaskDto> getTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @PathVariable("task_id") UUID taskId
+    ) {
+        return taskService.getTask(taskListId, taskId).map(taskMapper::toDto);
     }
 }
--- src/main/java/com/amariwyking/tasks/services/TaskService.java
+++ src/main/java/com/amariwyking/tasks/services/TaskService.java
@@ -3,9 +3,11 @@ package com.amariwyking.tasks.services;
 import com.amariwyking.tasks.domain.entities.Task;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TaskService {
     List<Task> listTasks(UUID taskListId);
     Task createTask(UUID taskListId, Task task);
+    Optional<Task> getTask(UUID taskListId, UUID taskId);
 }
